6ac9904ff18bfe91d9ecd766df2dad55cb21f3fb
urimu <munitzuri@gmail.com>
2020-01-15 19:08:31 +0200

    init commit

---
+++ public/js/app.js
@@ -0,0 +1,41 @@
+
+
+fetch('http://localhost:3000/weather?address=boston').then(
+    (response) => {
+        response.json().then((data) => {
+            if (data.error) {
+                return console.log(data.error)
+            }
+            console.log(data.location)
+            console.log(data.forecast)
+        })
+
+    })
+
+const weatherForm = document.querySelector('form')
+const searchElement = document.querySelector('input')
+const message1 = document.querySelector('#message1')
+const message2 = document.querySelector('#message2')
+
+
+
+weatherForm.addEventListener('submit', (e) => {
+    e.preventDefault()
+    
+    message1.textContent='loading...'
+    message2.textContent=''
+
+    fetch('http://localhost:3000/weather?address='+searchElement.value).then(
+    (response) => {
+        response.json().then((data) => {
+           
+            if (data.error) {
+                return message1.textContent=data.error
+            }
+            message1.textContent=data.location
+            message2.textContent=data.forecast
+        })
+
+    })
+//
+})
---
+++ src/app.js
@@ -0,0 +1,90 @@
+const path = require('path')
+const express = require('express')
+const hbs = require('hbs')
+const geoCode=require('./utils/geocode')
+const forecast=require('./utils/forecast')
+
+const app = express()
+const publicPath = path.join(__dirname, '../public')
+const viewsPath = path.join(__dirname, '../templates/views')
+const partials = path.join(__dirname, '../templates/partials')
+const name = 'Amit Munitz'
+app.set('view engine', 'hbs')
+app.set('views', viewsPath)
+hbs.registerPartials(partials)
+app.use(express.static(publicPath))
+
+
+app.get('', (req, res) => {
+    res.render('index', {
+        title: 'Weather app',
+        name
+    })
+})
+
+app.get('/about', (req, res) => {
+    res.render('about', {
+        title: 'About Me',
+        name
+    })
+})
+
+app.get('/help', (req, res) => {
+
+    res.render('help', {
+        title: 'help Me',
+        name
+    })
+})
+
+// app.get('/products', (req, res) => {
+//     if (!req.query.search) {
+//         return res.send({error : 'no search term'})
+//     } 
+//      res.send({ products: [] })
+
+// })
+
+app.get('/weather', (req, res) => {
+    if (!req.query.address){
+        return res.send({error:'no address'})
+    }
+    geoCode(req.query.address, (error,{longitude,latitude,location}={}) => {
+        if (error) {
+            return res.send({error})
+        }
+
+        forecast(latitude, longitude, (error, forecastData) => {
+            if (error) {
+                return res.send({error})
+            }
+
+            res.send({
+                forecast: forecastData,
+                location,
+                address: req.query.address
+            })
+        })
+    })
+
+    
+})
+app.get('/help/*', (req, res) => {
+    res.render('404', {
+        title: '404',
+        name,
+        errorMsg: 'help article not found'
+    })
+})
+
+app.get('*', (req, res) => {
+    res.render('404', {
+        title: '404',
+        name,
+        errorMsg: 'page not found'
+    })
+})
+
+app.listen(3000, () => {
+    console.log('Server is up on port 3000.')
+})
