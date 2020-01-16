c119d67ff03dfcc78417af74634a207fa152ffed
urimu <munitzuri@gmail.com>
2020-01-16 08:49:27 +0200

    added round to percent

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -1,4 +1,5 @@
 const request = require('request')
+//const Math = require ('Math')
 
 const forecast = (latitude, longitude, callback) => {
     const url = 'https://api.darksky.net/forecast/e0ab98f212979c3d4752ae773f16b5be/' + latitude + ',' + longitude+'?units=si'
@@ -9,7 +10,7 @@ const forecast = (latitude, longitude, callback) => {
         } else if (body.error) {
             callback('Unable to find location', undefined)
         } else {
-            callback(undefined, body.daily.data[0].summary + ' It is currently ' + body.currently.temperature + ' degrees out. There is a ' + body.currently.precipProbability*100 + '% chance of rain.')
+            callback(undefined, body.daily.data[0].summary + ' It is currently ' + body.currently.temperature + ' degrees out. There is a ' +  Math.round(body.currently.precipProbability*100) + '% chance of rain.')
         }
     })
 }
